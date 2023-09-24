4a2235ed99cbb119d2aa686a4c72d901f6060b6e
Rida Rhnizar <rrhnizar@c1r3p5.1337.ma>
2023-09-24 12:15:40 +0100

    i think finish ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 12:41:20 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/09/20 12:42:03 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/09/24 12:13:26 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,36 @@
 #define MUTANSTACK_HPP
 
 #include <iostream>
+#include <stack>
 
+template <typename T, class container=std::deque<T> >
 
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack(){}
+		MutantStack(MutantStack& otherMutantStack)
+		{
+			*this = otherMutantStack;
+		}
+		MutantStack& operator=(MutantStack& otherMutantStack)
+		{
+			if (this != &otherMutantStack)
+			{
+				
+			}
+			return *this;
+		}
+		~MutantStack(){}
+		typedef typename container::iterator iterator;
+		iterator begin()
+		{
+			return this->c.begin();
+		}
+		iterator end()
+		{
+			return this->c.end();
+		}
+};
 
 #endif
